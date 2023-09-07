1cd154cd937ce497ccc1e3cc08f0b0263a5dde21
LUO Sheng <luosheng1986@gmail.com>
2023-09-07 11:28:08 +0800

    Fix crash on gradle 7.0+

--- android/src/main/java/com/lewin/qrcode/QRScanReader.java
+++ android/src/main/java/com/lewin/qrcode/QRScanReader.java
@@ -12,7 +12,7 @@ import com.facebook.react.bridge.ReactMethod;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.common.Barcode;
 import com.google.mlkit.vision.barcode.BarcodeScanner;
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
 import com.google.mlkit.vision.barcode.BarcodeScanning;
@@ -27,7 +27,7 @@ import java.util.List;
  * Updated by stefanmajiros on 2021/6/15
  */
 
-public class QRScanReader extends ReactContextBaseJavaModule  {
+public class QRScanReader extends ReactContextBaseJavaModule {
 
     public QRScanReader(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -39,30 +39,30 @@ public class QRScanReader extends ReactContextBaseJavaModule  {
     }
 
     @ReactMethod
-    public void readerQR(String fileUrl, final Promise promise ) {
-        // ML Vision : https://developers.google.com/ml-kit/vision/barcode-scanning/android#java
+    public void readerQR(String fileUrl, final Promise promise) {
+        // ML Vision :
+        // https://developers.google.com/ml-kit/vision/barcode-scanning/android#java
         try {
             Uri uri = Uri.parse(fileUrl);
             InputImage image = InputImage.fromFilePath(this.getReactApplicationContext(), uri);
             BarcodeScannerOptions options = new BarcodeScannerOptions.Builder()
                     .setBarcodeFormats(
                             Barcode.FORMAT_AZTEC,
-                            Barcode.FORMAT_QR_CODE
-                    )
+                            Barcode.FORMAT_QR_CODE)
                     .build();
             final BarcodeScanner scanner = BarcodeScanning.getClient(options);
             Task<List<Barcode>> result = scanner.process(image)
                     .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
                         @Override
                         public void onSuccess(List<Barcode> barcodes) {
-                            Log.d("OK", " " +  barcodes.toString());
+                            Log.d("OK", " " + barcodes.toString());
                             List<String> rawValues = new LinkedList<>();
-                            for (Barcode barcode: barcodes) {
+                            for (Barcode barcode : barcodes) {
                                 String rawValue = barcode.getRawValue();
                                 rawValues.add(rawValue);
                             }
                             scanner.close();
-                            if (!rawValues.isEmpty()){
+                            if (!rawValues.isEmpty()) {
                                 promise.resolve(rawValues.get(0));
                             } else {
                                 promise.reject("NOT_OK", "Invalid or No related QR code");
@@ -73,7 +73,7 @@ public class QRScanReader extends ReactContextBaseJavaModule  {
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
-                            Log.d("NOT_OK", "" +  e.getMessage());
+                            Log.d("NOT_OK", "" + e.getMessage());
                             scanner.close();
                             promise.reject("NOT_OK", e.getMessage());
 
